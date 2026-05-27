a66539f3940730f611f40e51739d54ccd75bad6c
lfournea <lfournea@f4r7s3.paris.42.school>
2026-05-27 15:08:16 +0200

    save

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -1,3 +1,59 @@
 #include "Fixed.hpp"
 
-Fixed(void) : 
+Fixed::Fixed(void) : _fixedpoint(0) 
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(int const val)
+{
+	std::cout << "Int constructor called" << std::endl;
+	_fixedpoint = val * pow(2, fractionnal);
+}
+
+Fixed::Fixed(float const val)
+{
+	std::cout << "Float constructor called" << std::endl;
+	_fixedpoint = val * pow(2, fractionnal);
+}
+
+Fixed::Fixed(Fixed const &copy)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = copy;
+}
+
+Fixed &Fixed::operator=(Fixed const &other)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	_fixedpoint = other.getRawBits() * pow(2, fractionnal);
+	return (*this);
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+int	Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (_fixedpoint / pow(2, fractionnal)); 
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	this->_fixedpoint = raw; 
+}
+
+float	Fixed::toFloat(void) const
+{
+	return (_fixedpoint / pow(2, fractionnal));
+}
+
+int	Fixed::toInt(void) const
+{
+	return (_fixedpoint / pow(2, fractionnal));
+}
+
+int const	Fixed::fractionnal = 8;
