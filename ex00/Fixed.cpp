42e745634625c41d64f3ad74438c15c6b8caa6ba
lfournea <lfournea@f4r7s3.paris.42.school>
2026-05-27 13:11:02 +0200

    save

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -1,3 +1,37 @@
 #include "Fixed.hpp"
 
-Fixed(void) : 
+Fixed::Fixed(void) : _fixedpoint(0) 
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(Fixed const &copy)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = copy;
+}
+
+Fixed &Fixed::operator=(Fixed const &other)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	_fixedpoint = other.getRawBits() * pow(2, fractionnal);
+	return (*this);
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+int	Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (_fixedpoint / pow(2, fractionnal)); 
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	this->_fixedpoint = raw; 
+}
+
+int const	Fixed::fractionnal = 8;
--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -1,18 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lfournea <lfournea@learner.42.tech>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/27 12:36:37 by lfournea          #+#    #+#             */
+/*   Updated: 2026/05/27 12:53:55 by lfournea         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FIXED_HPP
 # define FIXED_HPP
+
+# include <cmath>
+# include <iostream>
 
 class	Fixed
 {
 public:
 	Fixed(void);
-	Fixed(Fixed copy);
+	Fixed(Fixed const &copy);
+	Fixed &operator=(const Fixed& other);
 	~Fixed(void);
-	int	Fixed::getRawBits(void) const;
-	int	Fixed::setRawBits(void);
+	int	getRawBits(void) const;
+	void	setRawBits(int const raw);
 private:
-	int	_fixed-point;
-}
-
-static int const	fractionnal;
+	int	_fixedpoint;
+	static int const	fractionnal;
+};
 
 #endif
